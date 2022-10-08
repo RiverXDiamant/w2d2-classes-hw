227ab817465f79d8ebe55cb9b9356a867d307d1f
Alex <eddings.sharanna@gmail.com>
2022-10-08 11:52:10 -0400

    first commit

---
+++ classes-homework.js
@@ -0,0 +1,51 @@
+// =====W2D2- Classes Homework
+
+// ===== Classes =====
+//  You will be working with this class
+//  class Cat {}
+//   a. give the class at least 3 properties
+//   b. give the class at least 3 methods
+//   c. create two instances of the class (two cats)
+//   d. log both instances to see their properties
+//   e. invoke each method from both instances
+
+// Classes are blueprints for objects
+
+// const lion = {
+//     legs: 4,
+//     color: "golden",
+//     diet: "carnivores"
+// }
+
+// const zebra = {
+//     legs: 4,
+//     color: "black and white",
+//     diet: "herbivore"
+// }
+
+// class is a reserved JS keyword
+// classes always start with a capital letter
+
+
+class Cat {
+    this.breed = breed,
+    
+}
+
+
+
+
+// ===== Constructors ====
+// here is a class to work with
+
+// class Pirate {
+
+//   constructor(){
+
+//   }
+
+// }
+//   a. create 3 properties that are set by the constructor
+//   b. create 3 methods
+//   c. instantiate /2 arrays/ of /3 pirates/, name the arrays after your favorite pirate ships, jollyRoger and blackPearl
+//   d. loop over each array and print 3 properties of each pirate
